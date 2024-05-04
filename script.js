5ad1c719219d64f96fd0ee53a1cb3775e371887f
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-05-04 19:17:10 +0300

    finish js

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-/*
-const greetings = ["Hello", "Hi", "Hey", "Greetings"];
-const adjectives = ["amazing", "wonderful", "fantastic", "incredible", "awesome"];
-const nouns = ["day", "evening", "morning", "afternoon", "week", "weekend"];
-const phrases = ["hope you're doing well", "enjoy your day", "have a good one", "take care"];""
 
-// generate a random message
-const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-
-// create the message
-const message = `${randomGreeting}! Have an ${randomAdjective} ${randomNoun}. ${randomPhrase}!`;
-
-// output the message to the console
-console.log(message);
-
-*/
-
+// Define the arrays and generate a random message
 const greetings = ['Hello', 'Hi', 'Hey', 'Greetings'];
 const adjectives = ['amazing', 'wonderful', 'fantastic', 'incredible', 'awesome'];
 const nouns = ['day', 'evening', 'morning', 'afternoon', 'week', 'weekend'];
 const phrases = ['hope you are doing well', 'enyoy your day', 'have a good one', 'take care'];
 
+// Get the button and the element to display the message
+const button = document.querySelector('.new-message');
+const quote = document.querySelector('.quote');
+const name = document.querySelector('.name');
 
+//Function to generate new message
+function generateMessage(){
 const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
 const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
 const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
 const randomPhrase= phrases[Math.floor(Math.random() * phrases.length)];
-
-
 const message = `${randomGreeting}! Have an ${randomAdjective} ${randomNoun}. ${randomPhrase}!`;
 
-console.log(message)
+// Upadate the quote and the name elements with the new message
+quote.textContent = message;
+name.textContent = 'Random Message Generator';
+}
+
+// Add an event listener to the button that will call the generateMessage function when clicked
+button.addEventListener('click', generateMessage);
+
+// Generate a message when the page loads
+//generateMessage();
+
 
 
 
